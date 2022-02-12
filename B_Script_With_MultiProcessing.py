2c96bc03a447dc6ad0c203311950430d5d87422d
Abhijit Kailas Gadhave <91112733+AbhiGadhave11@users.noreply.github.com>
2022-02-12 13:24:06 +0530

    Update B_Script_With_MultiProcessing.py

--- B_Script_With_MultiProcessing.py
+++ B_Script_With_MultiProcessing.py
@@ -11,6 +11,53 @@ import time
 import multiprocessing
 
 def Sending_Mail(data):
+	
+	with open("Birth_Image.jpg","rb") as f:
+		img_data = f.read();
+
+	msg = MIMEMultipart()
+	msg["From"] = "abhigadhave97@gmail.com"
+	msg["To"] = data[4]
+	msg['subject'] = "Happy Birthday(Wishes)"
+	body = "Hello "+data[1]+",\n"+"Wishing You a Very Very Wonderful birthday! May all your dreams comes true!!Have a great year ahead.💫️🤗️💥️🍫️💥️💥️"
+	msg.attach(MIMEText(body, 'plain'))
+
+	image = MIMEImage(img_data,name="Birth_Image")
+	msg.attach(image)
+
+	s = smtplib.SMTP('smtp.gmail.com', 587)
+	s.ehlo()
+	s.starttls()
+	s.ehlo()
+	
+	s.login("abhigadhave97@gmail.com", "Abhijitkg111")
+	
+	text = msg.as_string()
+	s.sendmail("abhigadhave97@gmail.com", data[4], text)
+	
+	s.quit()
+
+def Receive_Data(strX):
+	todayDate = date.today()
+	
+	stringD = (str)(todayDate)
+	x = stringD.split("-")
+	
+	DOB = x[1]+"-"+x[2]
+	
+	workbook = pd.read_excel(strX)
+	
+	for i in range(0,len(workbook.index)):
+		if(DOB == workbook['DOB'].iloc[i])
+		
+		wb = xlrd.open_workbook(strX)
+		sheet = wb.sheet_by_index(0)
+		data.append(sheet.row_values(i+1))
+		del wb
+		
+	pool = multiprocessing.pool()
+	pool.map(Sending_Mail,data)
+	pool.close()
 
 
 
